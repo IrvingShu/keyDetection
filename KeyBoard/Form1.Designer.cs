b2c9c50e1878ac4c58c2fdd300dffa02deb120b0
Young <810315118@qq.com>
2015-08-15 15:02:45 +0800

    detect key
    resizeimg

---
+++ KeyBoard/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace KeyBoard
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(75, 234);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(319, 152);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseWheel+=pictureBox1_MouseWheel;
+            this.pictureBox1.MouseClick+=pictureBox1_MouseClick;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.pictureBox2.Location = new System.Drawing.Point(75, 26);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(319, 152);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(758, 19);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(83, 16);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "open_image";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(758, 53);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(83, 16);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "select_ROI";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(758, 88);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(101, 16);
+            this.radioButton3.TabIndex = 4;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "draw contours";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(758, 128);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(95, 16);
+            this.radioButton4.TabIndex = 5;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "radioButton4";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(758, 162);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(95, 16);
+            this.radioButton5.TabIndex = 6;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "radioButton5";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(758, 234);
+            this.trackBar1.Maximum = 255;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(184, 45);
+            this.trackBar1.TabIndex = 7;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(776, 303);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(756, 209);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "binary";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(813, 206);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(57, 21);
+            this.textBox1.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 479);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.radioButton5);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ KeyBoard/Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace KeyBoard
+{
+    public partial class Form1 : Form
+    {
+        Image<Gray, Byte> imageGrayscale;
+        private Image<Gray, Byte> dest;
+        private Image<Rgb, Byte> src_color;
+        //private Image src_color;
+        private Image<Gray, Byte> dest_gray;
+        private Image<Gray, Byte> dest_binary;
+        private Image<Gray, Byte> dest_contours;
+        private Image<Gray, Byte> dest_rect;
+        private Image<Rgb, Byte> zoom_Img;
+
+
+        bool mouseClickFlag = false;
+
+        double zoomPara = 1.0F;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            Image img = Image.FromFile(@"C:\Users\Administrator\Desktop\11.png");
+            pictureBox1.Image = img;
+            
+                //!!!!!!!!!!!!!!!!!!!!!!!!!
+            
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            pictureBox1.MouseDown += new MouseEventHandler(pictureBox1_MouseDown);   //file:为鼠标移动定义一个事件处理过程"Form1_MouseMove"   
+            pictureBox1.MouseMove += new MouseEventHandler(pictureBox1_MouseMove);
+            pictureBox1.MouseUp += new MouseEventHandler(pictureBox1_MouseUp);
+
+     
+        }
+
+        private Point start = new Point();//矩形起点 
+        private Point end = new Point();//矩形终点 
+        private bool catchStart = false;
+        //鼠标按下   
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                if (!catchStart)
+                {
+                    catchStart = true;
+                    start.X = e.X;
+                    start.Y = e.Y;
+                    end.X = e.X;
+                    end.Y = e.Y;
+                }
+            }
+        }
+
+        
+        //鼠标释放  
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            catchStart = false;
+            Rectangle rect = new Rectangle(start.X, start.Y, end.X - start.X, end.Y - start.Y);
+            Image ChildImg = AcquireRectangleImage(pictureBox1.Image, rect);
+            Image<Rgb, Byte> imageSource = new Image<Rgb, byte>((Bitmap)ChildImg);
+            imageGrayscale = imageSource.Convert<Gray, Byte>();
+            pictureBox2.Image = imageGrayscale.ToBitmap();
+
+            dest_gray = imageGrayscale;//!!!!!!!!!!!!!!!!!!
+            dest_rect = dest_gray;
+
+            src_color = new Image<Rgb, byte>((Bitmap)ChildImg);
+        }
+
+        public static Image AcquireRectangleImage(Image source, Rectangle rect)
+        {
+            if (source == null || rect.IsEmpty) return null;
+            Bitmap bmSmall = new Bitmap(rect.Width, rect.Height, System.Drawing.Imaging.PixelFormat.Format32bppRgb);
+            //Bitmap bmSmall = new Bitmap(rect.Width, rect.Height, source.PixelFormat);            
+            using (Graphics grSmall = Graphics.FromImage(bmSmall))
+            {
+                grSmall.DrawImage(source, new System.Drawing.Rectangle(0, 0, bmSmall.Width, bmSmall.Height), rect, GraphicsUnit.Pixel);
+                grSmall.Dispose();
+            }
+            return bmSmall;
+        }
+        //鼠标移动   
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Graphics g = this.pictureBox1.CreateGraphics();
+
+            if (catchStart)
+            {
+                this.Refresh();
+                end.X = e.X;
+                end.Y = e.Y;
+                Rectangle rect = new Rectangle(start.X, start.Y, end.X - start.X, end.Y - start.Y);
+                g.DrawRectangle(new Pen(Color.White), rect);
+
+                //获取image的缩略图
+                // Image.GetThumbnailImageAbort myCallback =new Image.GetThumbnailImageAbort(ThumbnailCallback);
+                // Bitmap myBitmap = new Bitmap("Climber.jpg");
+                // Image myThumbnail = pictureBox1.Image.GetThumbnailImage(40, 40, myCallback, IntPtr.Zero);
+                // g.DrawImage(myThumbnail, 150, 75);
+                // Image *imgRect = img.GetThumbnailImage(end.X - start.X, end.Y - start.Y,null,null);
+            }
+        }
+
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            //trackBar1.ValueChanged += new (TrackBar_ValueChanged);
+            int iPos = trackBar1.Value;
+            Image<Gray, Byte> imageGray = new Image<Gray, Byte>(imageGrayscale.ToBitmap());//why not imageGrayscale?
+            dest_binary = new Image<Gray, Byte>(imageGray.Width, imageGray.Height);//these two sententences can not be removed to the out of  trackBar1_Scroll?
+            CvInvoke.cvThreshold(imageGray, dest_binary, iPos, 255, Emgu.CV.CvEnum.THRESH.CV_THRESH_BINARY);
+            pictureBox2.Image = dest_binary.ToBitmap();
+            textBox1.Text = Convert.ToString(iPos);
+
+
+            
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            
+          
+          //int n = CvInvoke.cvFindContours(dest,);
+          IntPtr Dyncontour = new IntPtr();
+          IntPtr Dynstorage = CvInvoke.cvCreateMemStorage(0);
+          int m = 88;
+          int n = CvInvoke.cvFindContours(dest_binary, Dynstorage, ref Dyncontour, m, Emgu.CV.CvEnum.RETR_TYPE.CV_RETR_EXTERNAL,Emgu.CV.CvEnum.CHAIN_APPROX_METHOD.CV_CHAIN_APPROX_NONE,new Point(0,0));
+          
+          Seq<Point> DyncontourTemp1 = new Seq<Point>(Dyncontour,null);
+          Seq<Point> DyncontourTemp = DyncontourTemp1;
+           for (; DyncontourTemp1 != null && DyncontourTemp1.Ptr.ToInt32() != 0; DyncontourTemp1 = DyncontourTemp1.HNext)
+           {
+
+               ;
+               Rectangle rect = CvInvoke.cvBoundingRect(DyncontourTemp1, 0);
+               CvInvoke.cvRectangleR(src_color, rect, new MCvScalar(255, 0, 0), 1, Emgu.CV.CvEnum.LINE_TYPE.CV_AA, 0);
+               //CvInvoke.cvDrawContours(dest, DyncontourTemp1, new MCvScalar(128, 128, 128), new MCvScalar(255, 255, 255), 0, 1, Emgu.CV.CvEnum.LINE_TYPE.CV_AA, new Point(0, 0));
+
+           }
+
+           pictureBox2.Image = src_color.ToBitmap();
+          //dest.Draw(bondContour, new MCvScalar(128,128,128),2);
+           
+
+   /*        Point point = new Point(1, 1);
+
+           Emgu.CV.Seq<System.Drawing.Point> qqq = null;
+
+           dest.Draw(, new Bgr(0,0,0), new Bgr(0,0,0), 2, 2);
+           Rectangle rect = CvInvoke.cvBoundingRect(bondContour,true);
+           Point leftPoint = new Point(rect.X,rect.Y);
+           Point rightPoint = new Point(rect.X + rect.Width, rect.Y + rect.Height); 
+           CvInvoke.cvRectangle(dest, leftPoint, rightPoint,new MCvScalar(250,0,0),1, Emgu.CV.CvEnum.LINE_TYPE.FOUR_CONNECTED, 0);
+           pictureBox2.Image = dest.ToBitmap();
+            
+            //Image<Gray,Byte> imageTemp=imageThreshold.Copy();
+            IntPtr storage = CvInvoke.cvCreateMemStorage(0);
+            IntPtr ptrFirstChain = IntPtr.Zero;
+            int total = CvInvoke.cvFindContours(dest.Ptr, storage, ref ptrFirstChain, 6553, Emgu.CV.CvEnum.RETR_TYPE.CV_RETR_LIST, Emgu.CV.CvEnum.CHAIN_APPROX_METHOD.CV_CHAIN_APPROX_SIMPLE, new Point(0, 0));
+
+            int aa = total;
+
+           */ // CvInvoke.cvEndFindContours();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        //mouse_wheel
+        private void pictureBox1_MouseWheel(object sender, MouseEventArgs e)
+        {
+            
+            //if (mouseClickFlag == true)
+            {
+                if (e.Delta > 0) 
+                { 
+                    label1.Text = "正在向上滚动滑轮";
+                    zoomPara += 0.05;
+                      
+                } 
+  
+                else
+                { 
+                    label1.Text = "正在向下滚动滑轮";
+                    zoomPara -= 0.05;
+
+                }
+                //Size img_size = new Size((int)(src_color.Width*zoomPara),(int)(src_color.Height*zoomPara));
+               // Bitmap zoom_Img = CvInvoke.cvCreateImage(img_size,Emgu.CV.CvEnum.IPL_DEPTH.IPL_DEPTH_8U,3);
+
+                Bitmap bit = new Bitmap((int)(src_color.Width * zoomPara), (int)(src_color.Height * zoomPara));
+                zoom_Img = new Image<Rgb, byte>(bit);
+                CvInvoke.cvResize(src_color, zoom_Img, Emgu.CV.CvEnum.INTER.CV_INTER_LINEAR);
+
+                pictureBox2.Image = zoom_Img.ToBitmap();
+
+             }
+        }
+
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            label1.Text = "1111";
+            this.pictureBox1.Focus();
+
+            if (mouseClickFlag == false)
+                mouseClickFlag = true;
+
+            else
+                mouseClickFlag = false;
+        }
+
+
+    }
+}
